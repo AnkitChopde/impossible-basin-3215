18ad5718f7299759bb1f1b54bf050222383741dc
Ankesh Chopde <acchopde84@gmail.com>
2023-01-18 23:58:55 +0530

    push

--- carters_clone/src/index.js
+++ carters_clone/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
-
+import {BrowserRouter} from "react-router-dom"
 const theme = extendTheme({
   primary:{
     color:"#444444",
@@ -17,8 +17,11 @@ const theme = extendTheme({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <ChakraProvider theme={theme}> 
       <App />
     </ChakraProvider>
+    </BrowserRouter>
+   
   </React.StrictMode>
 );
