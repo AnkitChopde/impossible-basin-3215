3d593024895452b4d1d778bca30f49bb174a5b38
Ankesh Chopde <acchopde84@gmail.com>
2023-01-20 08:46:43 +0530

    commited

--- carters_clone/src/Pages/Homepage.jsx
+++ carters_clone/src/Pages/Homepage.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
+import BelowNavbar from '../components/BelowNavbar'
 
 import Navbar from '../components/Navbar'
-import Carousal from '../Images&Logos/Carousal'
-import Girl from '../Images&Logos/Girl'
-
-
 const Homepage = () => {
   return (
     <div>
       
       <Navbar/>
-      <Girl/>
-     <Carousal/>
-      
+     <BelowNavbar/>
        {/* <Footer/> */}
     </div>
   )
---
+++ carters_clone/src/components/BelowNavbar.jsx
@@ -0,0 +1,13 @@
+import { Image } from '@chakra-ui/react'
+import React from 'react'
+import Pic2 from "../Images&Logos/Pic2.png"
+const BelowNavbar = () => {
+  return (
+    <div>
+      <Image mt={"12px"} src={Pic2} alt=""/>
+      
+    </div>
+  )
+}
+
+export default BelowNavbar
